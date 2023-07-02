e0aedfc14ae1aebe6f9c08cab8ca61d26905e8c6
i582 <51853996+i582@users.noreply.github.com>
2023-07-02 15:26:34 +0400

    project/flavors: added `'~/.local/bin/v'` as candidate for symlink

--- project/flavors/SymlinkToolchainFlavor.v
+++ project/flavors/SymlinkToolchainFlavor.v
@@ -1,21 +1,28 @@
 module flavors
 
 import os
+import utils
 
 pub struct SymlinkToolchainFlavor {}
 
 fn (s &SymlinkToolchainFlavor) get_home_page_candidates() []string {
-	symlink_path := '/usr/local/bin/v'
-	path_to_compiler := os.real_path(symlink_path)
-	if path_to_compiler == '' {
-		return []
+	symlink_path_candidates := ['/usr/local/bin/v', utils.expand_tilde_to_home('~/.local/bin/v')]
+
+	mut result := []string{}
+
+	for symlink_path_candidate in symlink_path_candidates {
+		path_to_compiler := os.real_path(symlink_path_candidate)
+		if path_to_compiler == '' {
+			continue
+		}
+
+		compiler_dir := os.dir(path_to_compiler)
+		if os.is_dir(compiler_dir) {
+			result << compiler_dir
+		}
 	}
 
-	compiler_dir := os.dir(path_to_compiler)
-	if os.is_dir(compiler_dir) {
-		return [compiler_dir]
-	}
-	return []
+	return result
 }
 
 fn (s &SymlinkToolchainFlavor) is_applicable() bool {
