4bb3f13562a1e9375a5270d37f2e1ab880c29e1a
i582 <51853996+i582@users.noreply.github.com>
2023-07-02 15:43:47 +0400

    project/flavors: check for `.bin` folder in PATH env

--- project/flavors/SysPathToolchainFlavor.v
+++ project/flavors/SysPathToolchainFlavor.v
@@ -9,6 +9,11 @@ fn (s &SysPathToolchainFlavor) get_home_page_candidates() []string {
 		.split(os.path_delimiter)
 		.filter(it != '')
 		.filter(os.is_dir)
+		.map(if os.file_name(it) == '.bin' {
+			os.dir(it)
+		} else {
+			it
+		})
 }
 
 fn (s &SysPathToolchainFlavor) is_applicable() bool {
