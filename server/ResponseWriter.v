a061eeeb67bc34cfdc14ffb07aacdbec05f38d87
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-12-10 20:45:25 +0100

    improve no result callback fix

--- server/ResponseWriter.v
+++ server/ResponseWriter.v
@@ -7,7 +7,6 @@ pub type ResponseWriter = jsonrpc.ResponseWriter
 
 fn (mut wr ResponseWriter) wrap_error(err IError) IError {
 	if err is none {
-		wr.write(jsonrpc.null)
 		return err
 	}
 	wr.log_message(err.msg(), .error)
